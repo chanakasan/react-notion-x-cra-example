294463fee4d89379fb10e188793e14858ba3afbf
Chanaka Abeysinghe <chanakasan@gmail.com>
2023-10-25 09:52:54 +0530

    initial react-notion-x cra example

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// TODO: change these to your own values
+// NOTE: rootNotionSpaceId is optional; set it to undefined if you don't want to
+// use it.
+export const rootNotionPageId = '2467a7e3cfd24b7ab868b9283061b733'
+export const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
